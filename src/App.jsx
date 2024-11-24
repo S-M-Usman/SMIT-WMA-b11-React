4a64d6504ade6ab44ca1a5c32a9c79cde8374b47
Shaikh Muhammad Usman <shaikhmuhammadusmanofficial@gmail.com>
2024-11-24 15:58:50 +0500

    updated code for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import "../Assets/style/style.css";
+import "../Assets/css/style.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
